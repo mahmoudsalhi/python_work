c0ee3ba671aaa2a3c08feaba42804c7dc803edbe
mahmoudsalhi <88332246+mahmoudsalhi@users.noreply.github.com>
2023-07-27 10:19:01 +0100

    Add files via upload

---
+++ index.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import messagebox
+
+window = tk.Tk()
+window.title("TIC-TAC-TOE")
+window.mainloop()
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+mark=""
+count=0
+panels=['panel',digits]
